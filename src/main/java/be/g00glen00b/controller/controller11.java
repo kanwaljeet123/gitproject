8302deb46779318abce1716955f19e2e93b44e0d
kanwal <kanwal@LAPTOP-3N8LG5FT>
2018-09-26 11:55:14 +0530

    mm

--- src/main/java/be/g00glen00b/controller/controller11.java
+++ src/main/java/be/g00glen00b/controller/controller11.java
@@ -2,8 +2,13 @@ package be.g00glen00b.controller;
 
 public class controller11 {
 public static void main(String[] args) {
+
 	System.out.println("welcome to github");
 	
+
+	System.out.println("hello123");
+	System.out.println("welcome");
+ 
 }
 
 }
